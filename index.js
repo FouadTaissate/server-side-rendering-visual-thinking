4e9093910ebf43916a57bdb45b69f77f173e8d06
FouadTaissate <112860051+FouadTaissate@users.noreply.github.com>
2023-04-03 12:09:26 +0200

    beign

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from "express";
+
+const url = "https://api.visualthinking.fdnd.nl/api/v1/";
+
+// Maak een nieuwe express app
+const app = express();
+
+// Stel in hoe we express gebruiken
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.static("public"));
+
+// Maak een route voor de index
+app.get("/", (request, response) => {
+  let methodsUrl = url + "/methods?first=100";
+  fetchJson(methodsUrl).then((data) => {
+    response.render("index", data);
+  });
+});
+
+app.get("/method/:slug", (request, response) => {
+  let detailPageUrl = url + "method/" + request.params.slug;
+  const id = request.query.id;
+  console.log(id);
+
+  fetchJson(detailPageUrl).then((data) => {
+    response.render("detail-page", data);
+  });
+});
+
+// Stel het poortnummer in en start express
+app.set("port", process.env.PORT || 8000);
+app.listen(app.get("port"), function () {
+  console.log(`Application started on http://localhost:${app.get("port")}`);
+});
+
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error);
+}
